e5925b6079678e2e47d286eafa4465f277f4687b
Filippo SMM <filippo.sm.matoso@gmail.com>
2017-09-06 21:14:00 +0100

    playing with tinderable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Tinderable from 'react-tinderable';
+import './style.css';
+
+var data = [
+  {
+    title: 'A wonderful day',
+    text: '—— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— -',
+    image: '',
+    id: '1'
+  },
+  {
+    title: 'My amazing journey',
+    text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ——— ———— ',
+    image: '',
+    id: '2'
+  },
+  {
+    title: 'Three recipes without cocoa',
+    text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ———',
+    image: '',
+    id: '3'
+  },
+  {
+    title: 'Generic clickbait title',
+    text: ' —— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— - ——— ',
+    image: '',
+    id: '4'
+  }
+];
+
+
 class App extends Component {
   render() {
     return (
@@ -10,12 +41,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Tinderable initialCardsData={data} />
       </div>
     );
   }
 }
+
 
 export default App;
